091c03f851e15bf1b728ad8d695b76efa5311028
Alexia Fauzia <alexiafauzia732@gmail.com>
2022-07-08 00:37:44 -0700

    Add files via upload

---
+++ GoogleKotlin/GoogleKotlin/app/src/main/java/mullatoez/pro/googlekotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package mullatoez.pro.googlekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
